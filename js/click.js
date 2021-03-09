bdb3a91f3af2a01dc98f35e8a6a5d1cb2863711c
Sakuemy <pointd58@gmail.com>
2021-03-09 12:16:57 +0300

    V 1.4

--- js/click.js
+++ js/click.js
@@ -1,7 +1,5 @@
 let tem_button = document.querySelector('.tem_button');
 let body = document.querySelector('body');
-let dropbtn = document.querySelector('.dropbtn');
-let dropdown_content = document.querySelector('.dropdown-content');
 
 function infoclick() {
     alert("Этот сайт предназначен для создания быстрых заметок. Все данные хранятся на Вашем компьютере и при очистке кэша браузера удаляются. \n\nСайт создан на коленке в 2021 году");
@@ -14,38 +12,14 @@ function donationclick() {
 function tem() {
     if (tem_button.textContent == 'Светлая тема'){
         tem_button.textContent = 'Тёмная тема';
-        body.style.backgroundColor = '#e2e2e2';
-        print.style.backgroundColor = '#ffffff';
-        print.style.color = '#000000';
-        dropbtn.style.backgroundColor = 'rgb(175 175 175)';
-        dropdown_content.style.backgroundColor = '#afafaf';
+        body.classList.remove('dark_theme');
+        body.classList.add('light_theme');
         localStorage.setItem('tem', '0');
     } else {
         tem_button.textContent = 'Светлая тема';
-        body.style.backgroundColor = '#252526';
-        print.style.backgroundColor = '#1e1e1e';
-        print.style.color = '#adadad';
-        dropbtn.style.backgroundColor = 'rgb(105 105 105)';
-        dropdown_content.style.backgroundColor = '#6b6968';
+        body.classList.remove('light_theme');
+        body.classList.add('dark_theme');
         localStorage.setItem('tem', '1');
-    }
-}
-
-function tem_stor() {
-    if (localStorage.getItem('tem') == '0'){
-        tem_button.textContent = 'Тёмная тема';
-        body.style.backgroundColor = '#e2e2e2';
-        print.style.backgroundColor = '#ffffff';
-        print.style.color = '#000000';
-        dropbtn.style.backgroundColor = 'rgb(175 175 175)';
-        dropdown_content.style.backgroundColor = '#afafaf';
-    } else {
-        tem_button.textContent = 'Светлая тема';
-        body.style.backgroundColor = '#252526';
-        print.style.backgroundColor = '#1e1e1e';
-        print.style.color = '#adadad';
-        dropbtn.style.backgroundColor = 'rgb(105 105 105)';
-        dropdown_content.style.backgroundColor = '#6b6968';
     }
 }
 
@@ -56,9 +30,20 @@ function clr() {
         if (confirm('Вы уверены что хотите очистить содержимое экрана? \nДанные будут удалены и из локального хранилища. \nДля подтверждения нажмите \"OK\", а для отмены \"Отмена\".')) {
             print.value = '';
             localStorage.setItem('areaText', print.value);
-        Autosize();
+            Autosize();
         }
     }
 }
 
-tem_stor();
+switch(localStorage.getItem('tem')) {
+
+    case '1':
+        tem_button.textContent = 'Тёмная тема';
+        tem();
+        break;
+
+    default:
+        tem_button.textContent = 'Светлая тема';
+        tem();
+        break;
+}
