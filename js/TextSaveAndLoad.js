eb68a15c644f45e0b0603b58a09779056f09507a
Sakuemy <pointd58@gmail.com>
2021-03-09 12:13:57 +0300

    V 1.1

--- js/TextSaveAndLoad.js
+++ js/TextSaveAndLoad.js
@@ -1,13 +1,23 @@
 let print = document.querySelector('.print');
+let hiddenDiv = document.querySelector('.hiddenDiv');
+
+//подгон размеров textarea под содержимое
+function Autosize(){
+    str = localStorage.getItem('areaText');
+    hiddenDiv.innerHTML = str.replace(new RegExp('\n', 'g'), '<br>');
+    print.style.height = hiddenDiv.offsetHeight + 'px';
+}
 
 //сохраняю в локалсторедж при любом изменении поля
 print.oninput = function () {
     localStorage.setItem('areaText', print.value);
+    Autosize();
 }
 
 //функция по загрузке из localstorage
 function onLoad () {
     print.value = localStorage.getItem('areaText');
+    Autosize();
 }
 
 onLoad();
