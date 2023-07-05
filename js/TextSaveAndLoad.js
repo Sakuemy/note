53aebfc4e8c4a8f62d0760f1619c1529a20b275f
root <root@ivanpc.localdomain>
2023-07-05 15:41:25 +0300

    Fix bug

--- js/TextSaveAndLoad.js
+++ js/TextSaveAndLoad.js
@@ -1,16 +1,22 @@
 let print = document.querySelector('.print');
 let hiddenDiv = document.querySelector('.hiddenDiv');
+let size = 0;
 
 //подгон размеров textarea под содержимое
 function Autosize(){
     str = localStorage.getItem('areaText');
     hiddenDiv.innerHTML = str.replace(new RegExp('\n', 'g'), '<br>');
-    print.style.height = hiddenDiv.offsetHeight + 'px';
+	size = 200 + hiddenDiv.offsetHeight
+    print.style.height = size + 'px';
 }
 
 //сохраняю в локалсторедж при любом изменении поля
 print.oninput = function () {
     localStorage.setItem('areaText', print.value);
+    Autosize();
+}
+
+print.onresize = function () {
     Autosize();
 }
 
